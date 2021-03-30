105d342db0d6756420e85a1256fccdcf44dfae98
张亚飞 <1739077662@qq.com>
2021-03-30 10:41:53 +0800

    init

---
+++ index.mjs
@@ -0,0 +1,24 @@
+import yup from "yup";
+
+let schema = yup.object().shape({
+  name: yup.string().required(),
+  age: yup.number().required().positive().integer(),
+  email: yup.string().email(),
+  website: yup.string().url(),
+  createdOn: yup.date().default(function () {
+    return new Date();
+  }),
+});
+
+// check validity
+(async () => {
+  try {
+    let res = await schema.validate();
+    console.log("res", res);
+    console.log("data", schema.getDefault());
+  } catch (error) {
+    console.log("error", error);
+  }
+})();
+
+// console.log(schema.cast());
